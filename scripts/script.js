c9b35c7fc343a3467613ee6fd2e15061b6a54534
mikosramek <miko@mikosramek.ca>
2019-11-19 10:38:00 -0500

    Adding pseudocode and sketch wireframe

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,25 @@
 const app = {};
 
+app.getRecs = () => {
+  //ajax call
+}
+
+app.showRecs = () => {
+  //display info on dom
+}
+
+app.showError = () => {
+  //display errors on around search bars
+  //display "no recs found" in results area
+}
+
+app.bindEvents = () => {
+  //search button
+  //search bar
+  //drop down filter
+}
+
+
 
 app.init = () => {
   console.log("Document Loaded");
